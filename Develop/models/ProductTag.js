8263adc9957f05f0355a68852cfc5268f4154c87
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-07-16 14:36:08 -0600

    added to index.js file under Model

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,8 +6,7 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
-    id: {
+        id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,35 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'cateogry_id',
+  onDelete: 'CASCADE',
+});
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'cateogry_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tags'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tags_forProduct'
+});
 
 module.exports = {
   Product,
