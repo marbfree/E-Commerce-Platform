36fa38ae2db9b1e234a4c2bb7a9f013553454456
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-07-17 12:06:23 -0600

    api routes written, save a couple

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -22,7 +22,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false
   },
-  as: 'product_tags'
+  as: 'product_id'
 })
 
 // Tags belongToMany Products (through ProductTag)
@@ -31,7 +31,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false
   },
-  as: 'tags_forProduct'
+  as: 'tag_id'
 });
 
 module.exports = {
